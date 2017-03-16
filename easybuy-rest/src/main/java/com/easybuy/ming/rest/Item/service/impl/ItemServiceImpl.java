7bf4d218d2c8cd5f15931f77da87193d261d6295
法号贫僧 <limingming_0305@163.com>
2017-03-16 23:26:48 +0800

    购物车

--- easybuy-rest/src/main/java/com/easybuy/ming/rest/Item/service/impl/ItemServiceImpl.java
+++ easybuy-rest/src/main/java/com/easybuy/ming/rest/Item/service/impl/ItemServiceImpl.java
@@ -4,12 +4,17 @@ import com.easybuy.ming.mapper.TbItemCatMapper;
 import com.easybuy.ming.pojo.TbItemCat;
 import com.easybuy.ming.pojo.TbItemCatExample;
 import com.easybuy.ming.rest.Item.service.ItemService;
+import com.easybuy.ming.rest.redis.service.JedisClient;
 import com.easybuy.ming.rest.util.pojo.ItemCatNode;
 import com.easybuy.ming.rest.util.pojo.ItemCatResult;
+import com.easybuy.ming.utils.JsonUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.annotation.Resource;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,8 +27,28 @@ public class ItemServiceImpl implements ItemService {
 
     @Autowired
     private TbItemCatMapper itemCatMapper;
+    @Resource
+    private JedisClient jedisClient;
+
+    @Value("${REDIS_EASYBUY_CAT_KEY=EASYBUY_CAT}")
+    private String REDIS_EASYBUY_CAT_KEY;
 
     public ItemCatResult getItemCatList() {
+        String cat="cat"; //目录
+        //添加缓存
+        //查询数据库之前先查询缓存，如果有直接返回
+        try {
+            //从redis中取缓存数据
+            String json = jedisClient.hget(REDIS_EASYBUY_CAT_KEY,"cat");
+            if (!StringUtils.isBlank(json)) {
+                //把json转换成List
+
+               // return provinces;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         //调用递归方法查询商品分类列表
         List catList = getItemCatList(0l);
         //返回结果
@@ -56,7 +81,7 @@ public class ItemServiceImpl implements ItemService {
                 resultList.add(node);
             } else {
                 //如果是叶子节点
-                String item = "/products/"+tbItemCat.getId()+".html|" + tbItemCat.getName();
+                String item = "|" + tbItemCat.getName();
                 resultList.add(item);
             }
         }
