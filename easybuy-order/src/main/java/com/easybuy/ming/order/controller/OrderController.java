7e117e94eb1ca0cbb25aa3a8c61d6a30ebd01a2e
法号贫僧 <limingming_0305@163.com>
2017-03-20 16:48:02 +0800

    单点登录

---
+++ easybuy-order/src/main/java/com/easybuy/ming/order/controller/OrderController.java
@@ -0,0 +1,11 @@
+package com.easybuy.ming.order.controller;
+
+import org.springframework.stereotype.Controller;
+/**
+ * Created by Administrator on 2017-03-20.
+ * 处理订单相关业务
+ */
+@Controller
+public class OrderController {
+
+}
