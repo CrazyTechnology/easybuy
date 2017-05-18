7863fbe90d73ec93d5cc82ad4e1bf35626e77fcd
法号贫僧 <limingming_0305@163.com>
2017-05-18 23:09:24 +0800

    购物车

---
+++ easybuy-common/src/main/java/com/easybuy/ming/utils/ConfigUtils.java
@@ -0,0 +1,24 @@
+package com.easybuy.ming.utils;
+
+/**
+ * Created by ming on 2017/5/13.
+ * 配置服务IP
+ */
+public class ConfigUtils {
+
+    //服务IP
+    public final static String REST_IP="http://localhost:8082/rest";
+
+    //登录IP
+    public final static String SSO_IP="http://locahost:8083/sso";
+
+    //订单系统
+    public final static String ORDER_IP="http://locahost:8083/order";
+
+    //搜索系统
+    public final static String SEARCH_IP="http://locahost:8083/search";
+
+    //购物车系统
+    public final static String CART_IP="http://locahost:8083/search";
+
+}
